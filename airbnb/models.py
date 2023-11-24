b42aaa98b5821cb6444d01559f49f9dbcfdb4acc
victormuthomi <victormuthomi100@gmail.com>
2023-11-24 12:32:57 +0300

    Added some fields on the modesls.

--- airbnb/models.py
+++ airbnb/models.py
@@ -11,16 +11,25 @@ class Airbnb(models.Model):
     avalability = models.BooleanField(default=True)
     services = models.TextField()
 
+    def __str__(self):
+          return self.name
+
 class Customer(models.Model):
      """Define the fields for the customers"""
      name = models.CharField(max_length=20)
      email = models.EmailField()
      phone_number = models.PositiveSmallIntegerField()
+
+     def __str__(self):
+          return self.name
      
 class Booking(models.Model):
      """Define the booking fields"""
      customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
      date = models.TimeField(auto_now_add=True)
+     airbnb = models.ForeignKey(Airbnb, on_delete=models.CASCADE, default=1)
+
+     
 
 
 
